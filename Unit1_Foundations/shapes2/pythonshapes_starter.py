f9d19ad22d9b02122225714dbbeb65dcf5d7a933
Blessing Ntekume <bntekume@yahoo.com>
2017-07-06 12:38:35 -0700

    Update pythonshapes_starter.py

--- Unit1_Foundations/shapes2/pythonshapes_starter.py
+++ Unit1_Foundations/shapes2/pythonshapes_starter.py
@@ -6,13 +6,17 @@ t = Turtle()
 
 # Set Up your screen and starting position.
 setup(500,300)
-x_pos = -250
-y_pos = -150
+x_pos = 0
+y_pos = 0
 t.setposition(x_pos, y_pos)
 
 ### Write your code below:
 
+pendown()
 
+for i in range(3):
+    forward(100) # go forward 100 steps
+    right(360/3)
 
 
 
