e49af25beb3b3485cf8f2762620c4eaa1f971127
Blessing Ntekume <bntekume@yahoo.com>
2017-07-10 09:42:47 -0700

    Update obamicon_starter.py

--- Unit1_Foundations/obamicon/obamicon_starter.py
+++ Unit1_Foundations/obamicon/obamicon_starter.py
@@ -7,13 +7,25 @@ lightBlue = (112, 150, 158)
 yellow = (252, 227, 166)
 
 # Import image.
-my_image = Image.open("IMAGENAME") #change IMAGENAME to the path on your computer to the image you're using
+my_image = Image.open("WonderW.jpg") #change IMAGENAME to the path on your computer to the image you're using
 image_list = my_image.getdata() #each pixel is represented in the form (red value, green value, blue value, transparency). You don't need the fourth value.
 image_list = list(image_list) #Turns the sequence above into a list. The list can be iterated through in a loop.
 
 recolored = [] #list that will hold the pixel data for the new image.
 
-#YOUR CODE to loop through the original list of pixels and build a new list based on intensity should go here.
+for pixel in image_list:
+    sum = pixel[0] + pixel[1] + pixel[2]
+    if sum < 182:
+        recolored.append(darkBlue)
+    elif 182 < sum < 364:
+        recolored.append(red)
+    elif 364 < sum < 546:
+        recolored.append(lightBlue)
+    else:
+        recolored.append(yellow)
+
+print(recolored)
+
 
 
 
@@ -22,3 +34,5 @@ new_image = Image.new("RGB", my_image.size) #Creates a new image that will be th
 new_image.putdata(recolored) #Adds the data from the recolored list to the image.
 new_image.show() #show the new image on the screen
 new_image.save("recolored.jpg", "jpeg") #save the new image as "recolored.jpg"
+
+
